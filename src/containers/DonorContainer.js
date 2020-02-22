4ef799b2f7d0a8d537cfc61cdbbef095aead795c
vyacheslav-code <vyacheslav.code@gmail.com>
2020-02-22 19:09:16 +0300

    donor view, recepient view blocks

--- src/containers/DonorContainer.js
+++ src/containers/DonorContainer.js
@@ -3,8 +3,32 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     return (
-      <div>
-        donor interface
+      <div className="donor-container">
+        <div className="donor-container--header">
+          <div className="donor-container--header__avatar">
+
+          </div>
+          <div className="donor-container--header__name">
+
+          </div>
+          <div className="donor-container--header__switch">
+
+          </div>
+        </div>
+        <div className="donor-container--main">
+          {/*if isNew || isBeingModerated*/}
+          <div className="donor-container--main__placeholder">
+
+          </div>
+          {/*if ready*/}
+          <div className="donor-container--main__data">
+
+          </div>
+          {/*disabled if isBeingModerated*/}
+          <div className="donor-container--main__button">
+
+          </div>
+        </div>
       </div>
     )  
   }
--- src/containers/RecepientContainer.js
+++ src/containers/RecepientContainer.js
@@ -3,8 +3,43 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     return (
-      <div>
-        recepient interface
+      <div className="recepient-container">
+        <div className="recepient-container--header">
+          <div className="recepient-container--header__details">
+            <div className="recepient-container--header__avatar">
+
+            </div>
+            <div className="recepient-container--header__name">
+
+            </div>
+          </div>
+          <div className="recepient-container--header__id">
+
+          </div>
+        </div>
+        <div className="recepient-container--main">
+          {/*if list.length === 0*/}
+          <div className="recepient-container--main__placeholder">
+
+          </div>
+          {/* if list */} 
+          <div className="recepient-container--main__list">
+            <div className="recepient-container--main__list-item">
+              <div className="recepient-container--main__list-item-avatar">
+
+              </div>
+              <div className="recepient-container--main__list-item-name">
+
+              </div>
+              <div className="recepient-container--main__list-item-tip">
+
+              </div>
+            </div>
+          </div>
+          <div className="recepient-container--button">
+            
+          </div>
+        </div>
       </div>
     )  
   }
